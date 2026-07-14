a02cc5b52f3cff854e2b57219355eefa0fe69265
aryan01 <270636831+zerodayaryan@users.noreply.github.com>
2026-07-14 19:04:07 +0530

    handle IPv6 prackets and IPv4 packets differently

--- sniffer.py
+++ sniffer.py
@@ -1,27 +1,27 @@
 #Use scapy to capture and parse packets on your own network interface. Log source/destination IPs, protocols, and payload sizes. 
 # Good next step: build a simple dashboard (matplotlib or a live terminal UI) showing traffic by protocol.
 import scapy.all as scapy
+import json
 
 def capPackets():
     packets={}
-    pkts = scapy.sniff(count = 5, iface="en0")
-    prtcl = 0
-    for i in range(5):
+    pkts = scapy.sniff(count = 5)
+    for i in range(len(pkts)):
         if "IP" in pkts[i]:
+            prtcl = pkts[i]["IP"].proto
             srcIP=pkts[i]["IP"].src
             destIP=pkts[i]["IP"].dst
             size = pkts[i]["IP"].len
-            if pkts[i]["IP"].proto == 6:
-                prtcl = "TCP"
-            elif pkts[i]["IP"].proto == 17:
-                prtcl = "UDP"
-            elif pkts[i]["IP"].proto == 1:
-                prtcl = "ICMP"
-            else:
-                prtcl = "Other Protocol"
-            packets[pkts[i]["IP"].id]={"srcIp":srcIP, "destIP":destIP, "payload size": size, "protocol": prtcl}
-        else:
-            print("Not an IP packet")
+            packets[i]={"type": "IP","srcIp":srcIP, "destIP":destIP, "payload size": size, "protocol": prtcl}
+        elif "IPv6" in pkts[i]:
+            srcIP=pkts[i]["IPv6"].src
+            destIP=pkts[i]["IPv6"].dst
+            size = pkts[i]["IPv6"].plen
+            prtcl = pkts[i]["IPv6"].nh
+            packets[i]={"type": "IPv6", "srcIp":srcIP, "destIP":destIP, "payload size": size, "protocol": prtcl}
+    with open("output.json", "w") as json_file:
+        json.dump(packets, json_file, indent=4)
     print(packets)
+    
 
 capPackets()
