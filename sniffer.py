c20672d115c192100b5cda525d63bce2fac1a49b
aryan01 <270636831+zerodayaryan@users.noreply.github.com>
2026-07-19 00:35:52 +0530

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -1,37 +1,95 @@
-import scapy.all as scapy
-import json
 import argparse
+import json
+import scapy.all as scapy
 
-def get_args():
+def get_args():  # argument parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i","-interface", dest = "interface", help = "Enter the interface name")
-    parser.add_argument("-c","-count", dest = "count",type = int, help="Enter how many packets to capture. 0 represents infinite packets")
+    parser.add_argument(
+        "-i",
+        "-interface",
+        type=str,
+        dest="interface",
+        help="Enter the interface name",
+        required=True,
+    )
+    parser.add_argument(
+        "-c",
+        "-count",
+        dest="count",
+        type=int,
+        help="Enter how many packets to capture. 0 represents infinite packets. Default value is  5",
+        default=5,
+    )
     options = parser.parse_args()
-    if not options.interface or not options.count:
+    if not options.interface:
         parser.error("[-] Please enter valid interface")
-        
+
     return options.interface, options.count
-    
-def capPackets(interface, count):
-    packets={}
-    pkts = scapy.sniff(count = count, iface = interface)
+
+
+def capPackets(interface, count):  # function to capture packets
+    packets = {}
+    pkts = scapy.sniff(count=count, iface=interface)
     for i in range(len(pkts)):
         if "IP" in pkts[i]:
-            prtcl = pkts[i]["IP"].proto
-            srcIP=pkts[i]["IP"].src
-            destIP=pkts[i]["IP"].dst
-            size = pkts[i]["IP"].len
-            packets[i]={"type": "IP","srcIp":srcIP, "destIP":destIP, "payload size": size, "protocol": prtcl}
+            parseIP(pkts, i, packets)
         elif "IPv6" in pkts[i]:
-            srcIP=pkts[i]["IPv6"].src
-            destIP=pkts[i]["IPv6"].dst
-            size = pkts[i]["IPv6"].plen
-            prtcl = pkts[i]["IPv6"].nh
-            packets[i]={"type": "IPv6", "srcIp":srcIP, "destIP":destIP, "payload size": size, "protocol": prtcl}
+            parseIP6(pkts, i, packets)
+        storePacket(packets)
+
+
+def parseIP(pkts, i, packets):  # Function to parse IPv4 packets
+    packets[i] = {
+        "type": "IP",
+        "srcIp": pkts[i]["IP"].src,
+        "destIP": pkts[i]["IP"].dst,
+        "payload size": pkts[i]["IP"].len,
+    }
+    if "UDP" in pkts[i]:
+        packets[i].update(udpPkt(pkts, i))
+    elif "TCP" in pkts[i]:
+        packets[i].update(tcpPkt(pkts, i))
+
+
+def parseIP6(pkts, i, packets):  # Function to parse IPV6 packets
+    packets[i] = {
+        "type": "IPv6",
+        "srcIp": pkts[i]["IPv6"].src,
+        "destIP": pkts[i]["IPv6"].dst,
+        "payload size": pkts[i]["IPv6"].plen,
+    }
+    if "UDP" in pkts[i]:
+        packets[i].update(udpPkt(pkts, i))
+    elif "TCP" in pkts[i]:
+        packets[i].update(tcpPkt(pkts, i))
+    return packets
+
+
+def udpPkt(pkts, i):
+    UDP = {
+        "Protocol": "UDP",
+        "Src": pkts[i]["UDP"].sport,
+        "Dst": pkts[i]["UDP"].dport,
+        "len": pkts[i]["UDP"].len,
+    }
+    return UDP
+
+
+def tcpPkt(pkts, i):
+    tcp = {
+        "Protocol": "TCP",
+        "Src": pkts[i]["TCP"].sport,
+        "Dst": pkts[i]["TCP"].dport,
+        "Ack": pkts[i]["TCP"].ack,
+    }
+    return tcp
+
+
+def storePacket(packet):  # function to store parsed packets in json format
     with open("output.json", "w") as json_file:
-        json.dump(packets, json_file, indent=4)
-    print(packets)
-    
+        json.dump(packet, json_file, indent=4)
+        print(packet)
+
 
 interface, count = get_args()
 cap = capPackets(interface, count)
