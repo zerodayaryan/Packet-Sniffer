bb7948dbb7c219ff4c3968ea63e97ca569e908a2
aryan01 <270636831+zerodayaryan@users.noreply.github.com>
2026-08-18 00:15:24 +0530

    update

--- sniffer.py
+++ sniffer.py
@@ -1,54 +1,79 @@
+##Lots of work to do ;)
+## fix packet storing in json file
+## netwok instrution detection system
 import argparse
-import json
 import scapy.all as scapy
-import threading
+from rich.live import Live
+from rich.table import Table  
+from rich.console import Console
+import json
 
-def get_args():  # argument parser
+console = Console()
+
+def buildTable():
+    table = Table(title="Live Packet Capture")
+    table.add_column("Type")
+    table.add_column("Src IP")
+    table.add_column("Dst IP")
+    table.add_column("Protocol")
+    table.add_column("Details")
+    return table
+
+packet_table = buildTable()
+
+def parsePkt(pkts):
+    packets = {}
+    if "IP" in pkts:
+        parseIP(pkts, packets)
+    elif "IPv6" in pkts:
+        parseIP6(pkts, packets)
+    else:
+        return
+    
+
+    packet_table.add_row(
+        packets.get("type", ""),
+        str(packets.get("srcIp", "")),
+        str(packets.get("destIP", "")),
+        packets.get("Protocol", ""),
+        str({k: v for k, v in packets.items() if k not in ("type", "srcIp", "destIP", "Protocol")})
+    )
+    return packets
+
+def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-i",
-        "-interface",
+        "-i", "--interface",
         type=str,
         dest="interface",
         help="Enter the interface name",
         required=True,
     )
     parser.add_argument(
-        "-c",
-        "-count",
+        "-c", "--count",
         dest="count",
         type=int,
-        help="Enter how many packets to capture. 0 represents infinite packets. Default value is  5",
+        help="Enter how many packets to capture. 0 represents infinite packets. Default value is 5",
         default=5,
     )
     options = parser.parse_args()
     if not options.interface:
         parser.error("[-] Please enter valid interface")
-
     return options.interface, options.count
 
+def capPackets(interface, count):
+    with Live(packet_table, console=console, refresh_per_second=4):
+        scapy.sniff(count=count, iface=interface, prn=parsePkt)
 
-def capPackets(interface, count):  # function to capture packets
-    pkts = scapy.sniff(count=count, iface=interface, prn=parsePkt)
 
-def parsePkt(pkts):
-    packets = {}
-    if "IP" in pkts:
-        parseIP(pkts, packets)
-        print(packets, flush=True)
-        return packets
-    elif "IPv6" in pkts:
-        parseIP6(pkts, packets)
-        print(packets, flush=True)
-        return packets
 
-    # storePacket(packets)
-    
-def parseIP(pkts, packets):  # Function to parse IPv4 packets
+def parseIP(pkts, packets):
+    srcPkt = pkts["IP"].src
+    dstPkt = pkts["IP"].dst
     packets.update({
         "type": "IP",
-        "srcIp": pkts["IP"].src,
-        "destIP": pkts["IP"].dst,
+        "srcIp": srcPkt,
+        "destIP": dstPkt,
         "payload size": pkts["IP"].len,
     })
     if "UDP" in pkts:
@@ -57,12 +82,16 @@ def parseIP(pkts, packets):  # Function to parse IPv4 packets
         packets.update(tcpPkt(pkts))
     elif "ICMP" in pkts:
         packets.update(icmpPkt(pkts))
+    return packets
 
-def parseIP6(pkts, packets):  # Function to parse IPV6 packets
+
+def parseIP6(pkts, packets):
+    srcPkt = pkts["IPv6"].src
+    dstPkt = pkts["IPv6"].dst
     packets.update({
         "type": "IPv6",
-        "srcIp": pkts["IPv6"].src,
-        "destIP": pkts["IPv6"].dst,
+        "srcIp": srcPkt,
+        "destIP": dstPkt,
         "payload size": pkts["IPv6"].plen,
     })
     if "UDP" in pkts:
@@ -75,66 +104,59 @@ def parseIP6(pkts, packets):  # Function to parse IPV6 packets
 
 
 def udpPkt(pkts):
-    UDP = {
+    return {
         "Protocol": "UDP",
         "Src": pkts["UDP"].sport,
         "Dst": pkts["UDP"].dport,
         "len": pkts["UDP"].len,
     }
-    return UDP
 
 
 def tcpPkt(pkts):
-    tcp = {
+    return {
         "Protocol": "TCP",
         "Src": pkts["TCP"].sport,
         "Dst": pkts["TCP"].dport,
         "Ack": pkts["TCP"].ack,
     }
-    return tcp
+
 
 def icmpPkt(pkts):
-    if pkts["ICMP"].type == 0 or pkts["ICMP"].type == 8:
-        icmp = {
-            "Protocol":"ICMP - Query message",
-            "Type":pkts["ICMP"].type,
-            "ID":pkts["ICMP"].id,
-            "Sequence":pkts["ICMP"].seq,
-            "Payload":pkts["ICMP"].length
+    icmp_type = pkts["ICMP"].type
+    if icmp_type in (0, 8):
+        return {
+            "Protocol": "ICMP - Query message",
+            "Type": icmp_type,
+            "ID": pkts["ICMP"].id,
+            "Sequence": pkts["ICMP"].seq,
+            "Payload": pkts["ICMP"].length,
         }
-        return icmp
-    elif pkts["ICMP"].type == 3 or pkts["ICMP"].type == 5 or pkts["ICMP"].type == 11 or pkts["ICMP"].type == 12:
-        icmp={
-            "Protocol":"ICMP - Error message",
-            "Type":pkts["ICMP"].type,
-            "Code":pkts["ICMP"].code
+    elif icmp_type in (3, 5, 11, 12):
+        return {
+            "Protocol": "ICMP - Error message",
+            "Type": icmp_type,
+            "Code": pkts["ICMP"].code,
         }
-        return icmp
-    elif pkts["ICMP"].type == 13 or pkts["ICMP"].type == 14:
-        icmp = {
-            "Protocol":"ICMP - Query message",
-            "Type":pkts["ICMP"].type,
-            "Code":pkts["ICMP"].code,
-            "ID":pkts["ICMP"].id,
-            "Sequence":pkts["ICMP"].seq,
-            "Origin timestamp":pkts["ICMP"].ts_ori,
-            "Recieve timestamp":pkts["ICMP"].ts_rx,
-            "Transmit timestamp":pkts["ICMP"].ts_tx,
+    elif icmp_type in (13, 14):
+        return {
+            "Protocol": "ICMP - Query message",
+            "Type": icmp_type,
+            "Code": pkts["ICMP"].code,
+            "ID": pkts["ICMP"].id,
+            "Sequence": pkts["ICMP"].seq,
+            "Origin timestamp": pkts["ICMP"].ts_ori,
+            "Recieve timestamp": pkts["ICMP"].ts_rx,
+            "Transmit timestamp": pkts["ICMP"].ts_tx,
         }
-        return icmp
     else:
-        icmp={
-            "Protocol":"ICMP - Other/Unknown",
-            "Type":pkts["ICMP"].type
+        return {
+            "Protocol": "ICMP - Other/Unknown",
+            "Type": icmp_type,
         }
-        return icmp
 
-        
-# def storePacket(packet):  # function to store parsed packets in json format
-#     with open("output.json", "w") as json_file:
-#         json.dump(packet, json_file, indent=4)
-#         print(packet)
-
+def storePacket(packet):  # function to store parsed packets in json format
+    with open("output.json", "w") as json_file:
+        json.dump(packet, json_file, indent=4)
 
 interface, count = get_args()
 capPackets(interface, count)
